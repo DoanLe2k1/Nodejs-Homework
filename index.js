1ff7cef77c87897f33e9b777cf4c5e8063503c4e
Lê Minh Anh Đoàn <doanle2k1@gmail.com>
2024-12-02 01:33:10 +0700

    Fix something

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const router = require("./router/index")
 const createServer = require("http").createServer;
 
-const server = createServer((req, res) => {
-    router.run(req, res);
-});
+function handleServer(request, response) {
+    router.run(request, response);
+}
+const server = createServer(handleServer);
 
 server.listen(3001, "127.0.0.1", () => {
     console.log("Server is online at 127.0.0.1:3001")
